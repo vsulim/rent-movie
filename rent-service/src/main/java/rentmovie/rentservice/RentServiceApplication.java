cbe43e73e0336ba807ea90bcad8aee5822323725
vbertie <38598581+vbertie@users.noreply.github.com>
2018-09-29 14:51:35 +0200

    deleted unnecessary files

--- rent-service/src/main/java/rentmovie/rentservice/RentServiceApplication.java
+++ rent-service/src/main/java/rentmovie/rentservice/RentServiceApplication.java
@@ -2,11 +2,14 @@ package rentmovie.rentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 @EnableFeignClients("rentmovie.rentservice")
 public class RentServiceApplication {
 
